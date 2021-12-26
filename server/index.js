0848819d14b58696aca490e890a866e4d7a31a89
John Curry <fastpenguin91@gmail.com>
2021-12-26 10:22:37 -0800

    jwt malformed but verifyToken added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
 const { resolvers } = require("./src/resolvers");
+const { verifyToken } = require("./src/utils/verifyToken");
 
 const typeDefs = gql`
   type Todo {
@@ -20,6 +21,23 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req, ...res }) => {
+    let isAuthenticated = false;
+    let user = null;
+
+    try {
+      const authHeader = req.headers.authorization || "";
+
+      if (authHeader) {
+        const token = authHeader.split(" ")[1];
+
+        const payload = await verifyToken(token);
+
+        console.log("payload: ", payload);
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+
     console.log("headers auth: ", req.headers.authorization);
   },
 });
