94edc4a46910df00df1038dc8ecfd4084d8f7eee
John Curry <fastpenguin91@gmail.com>
2021-12-26 11:04:34 -0800

    token verified. valid payload returns

--- ui/src/auth/auth0-provider-with-history.js
+++ ui/src/auth/auth0-provider-with-history.js
@@ -3,12 +3,14 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const Auth0ProviderWithHistory = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
+      audience={audience}
     >
       {children}
     </Auth0Provider>
